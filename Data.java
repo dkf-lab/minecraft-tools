075ab64e25e0f638b62e67db66cd758067516c89
jonathan sawers <65479132+dkf-lab@users.noreply.github.com>
2023-01-07 13:55:01 +1030

    Create Data.java

---
+++ Data.java
@@ -0,0 +1,43 @@
+    // Data Management
+    private FileConfiguration dataConfig = null;
+    private File configFile = null;
+    public void reloadConfig() {
+        // creates config, checks for yml issues
+        if (this.configFile == null) {
+            configFile = new File(this.main.getDataFolder(), "mines.yml");
+        }
+        dataConfig = YamlConfiguration.loadConfiguration(configFile);
+        InputStream defaultStream = this.main.getResource("mines.yml");
+        if (defaultStream != null) {
+            YamlConfiguration defaultConfig = YamlConfiguration.loadConfiguration(new InputStreamReader(defaultStream));
+            this.dataConfig.setDefaults(defaultConfig);
+        }
+    }
+
+    public FileConfiguration getConfig() {
+        if (dataConfig == null) {
+            reloadConfig();
+        }
+        return dataConfig;
+    }
+
+    public void saveConfig() {
+        // save config after changing data
+        if (dataConfig == null||configFile==null) {
+            return;
+        }
+        try {
+            getConfig().save(this.configFile);
+        } catch (IOException e) {
+            main.getLogger().log(Level.SEVERE, "Could not save config to " + this.configFile, e);
+        }
+    }
+
+    public void saveDefaultConfig() {
+        if (this.configFile == null) {
+            configFile = new File(this.main.getDataFolder(), "mines.yml");
+        }
+        if (!configFile.exists()) {
+            main.saveResource("mines.yml", false);
+        }
+    }
